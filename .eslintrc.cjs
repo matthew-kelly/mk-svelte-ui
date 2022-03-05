396dd15cbf96cb0304f5e6415a50b7db8e5c6a4b
Matt Kelly <bmatthewkelly@gmail.com>
2022-03-05 09:23:23 -0800

    install tailwindcss

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
 	root: true,
-	extends: ['eslint:recommended', 'prettier'],
-	plugins: ['svelte3'],
+	extends: ['eslint:recommended', 'prettier', 'plugin:tailwindcss/recommended'],
+	plugins: ['svelte3', 'tailwindcss'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	parserOptions: {
 		sourceType: 'module',

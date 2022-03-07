72f5ee43b993bebad0ec941ea9fd5c00b560e48c
Matt Kelly <bmatthewkelly@gmail.com>
2022-03-06 23:33:58 -0800

    add global css reset, create Toggle component

---
+++ src/lib/Toggle.svelte
@@ -0,0 +1,80 @@
+<script>
+  export let label = '';
+  export let isToggled = false;
+  export let backgroundColor;
+  export let backgroundColorChecked;
+  export let buttonColor;
+
+  let styleString = '';
+  if (backgroundColor) styleString += `--toggleBackgroundColor: ${backgroundColor}; `;
+  if (backgroundColorChecked)
+    styleString += `--toggleBackgroundColorChecked: ${backgroundColorChecked}; `;
+  if (buttonColor) styleString += `--toggleButtonColor: ${buttonColor}; `;
+
+  function handleKeyDown(event) {
+    if (event instanceof KeyboardEvent && event.key !== 'Enter' && event.key !== ' ') {
+      return;
+    }
+    console.log(event);
+    return (isToggled = !isToggled);
+  }
+</script>
+
+<label style={styleString}>
+  <input type="checkbox" bind:checked={isToggled} />
+  <div
+    class="toggle"
+    tabindex="0"
+    role="button"
+    aria-pressed={isToggled}
+    on:keydown={handleKeyDown}
+  />
+  {label}
+</label>
+
+<style>
+  label {
+    --width: 40px;
+    --height: calc(var(--width) / 2);
+    --radius: calc(var(--height) / 2);
+    display: flex;
+    line-height: calc(var(--height) * 1.1);
+  }
+
+  .toggle {
+    position: relative;
+    width: var(--width);
+    height: var(--height);
+    border-radius: var(--radius);
+    border: solid 1px #777;
+    transition: background-color 0.2s ease;
+    margin-right: 5px;
+    background-color: var(--toggleBackgroundColor, #fff);
+    outline-width: 2px;
+  }
+
+  .toggle::after {
+    content: '';
+    position: absolute;
+    top: -1px;
+    left: -1px;
+    height: var(--height);
+    width: var(--height);
+    border-radius: var(--radius);
+    background-color: var(--toggleButtonColor, #ddd);
+    box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.5);
+    transition: transform 0.2s ease;
+  }
+
+  input {
+    display: none;
+  }
+
+  input:checked + .toggle {
+    background-color: var(--toggleBackgroundColorChecked, #00ff00);
+  }
+
+  input:checked + .toggle::after {
+    transform: translate3d(100%, 0, 0);
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Toggle from '$lib/Toggle.svelte';
+  let isToggled = true;
+  let isToggled2 = false;
+</script>
+
+<Toggle bind:isToggled />
+<Toggle
+  bind:isToggled
+  backgroundColor="orange"
+  buttonColor="blue"
+  backgroundColorChecked="pink"
+  label="Custom"
+/>
+
+{#if isToggled}
+  <h1>I'm toggled</h1>
+{/if}

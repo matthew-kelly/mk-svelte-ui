c2004500b7537b5669cb18ce83c6da5d2b69d46e
Matt Kelly <bmatthewkelly@gmail.com>
2022-03-07 17:58:38 -0800

    update Modal component to handle clicking outside

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -1,8 +1,10 @@
 <script>
+  import { clickOutside } from './clickOutside';
   import { fly, fade } from 'svelte/transition';
   import Portal from './Portal.svelte';
 
   export let isModalOpen = false;
+  export let background = true;
 
   function closeModal() {
     isModalOpen = false;
@@ -11,23 +13,30 @@
 
 {#if isModalOpen}
   <Portal>
-    <div class="modal" transition:fly={{ opacity: 0, y: 100 }}>
-      <button on:click={closeModal} aria-label="close modal box" type="button">Close</button>
+    <div
+      class="modal"
+      use:clickOutside
+      on:click-outside={closeModal}
+      transition:fly={{ opacity: 0, y: 100 }}
+    >
+      <button class="close-btn" on:click={closeModal} aria-label="close modal box" type="button"
+        >&times;</button
+      >
       <slot />
     </div>
-    <div class="background" on:click={closeModal} transition:fade />
+    {#if background}
+      <div class="background" on:click={closeModal} transition:fade />
+    {/if}
   </Portal>
 {/if}
 
 <style>
   .modal {
     position: fixed;
-    inset: 100px 0 0;
-    min-width: 320px;
+    inset: 100px 20px 0;
     max-width: 600px;
     margin: 0 auto;
     padding: 2rem;
-    width: 100%;
     z-index: 101;
     background-color: white;
     box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);
@@ -40,5 +49,11 @@
     inset: 0;
     position: fixed;
     z-index: 100;
+  }
+
+  .close-btn {
+    position: absolute;
+    top: 10px;
+    right: 10px;
   }
 </style>
